d63767577993aa63dd23fbfe07d9294a259a74a6
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-17 09:31:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn = document.querySelector('input[type="button"]');
+btn.addEventListener("click",function() {
+	let sel = document.getElementById("colorSelect");
+	let dropdown = sel.options[sel.selectedIndex];
+
+	if(dropdown) {
+		sel.remove(sel.selectedIndex);
+	}
+});
